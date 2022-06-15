199aa54a871a4eb814cd3829ae456dd7b4a7d7f2
Yavuz Selim KAHRAMAN <yskahraman@gmail.com>
2022-06-15 06:39:46 +0300

    Add project files.

---
+++ UdemyJwtApp.Back/Core/Domain/Category.cs
@@ -0,0 +1,14 @@
+﻿namespace UdemyJwtApp.Back.Core.Domain
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string? Definition { get; set; }
+        public List<Product> Products { get; set; }
+
+        public Category()
+        {
+            Products = new List<Product>();
+        }
+    }
+}
---
+++ UdemyJwtApp.Back/Persistance/Context/UdemyJwtContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UdemyJwtApp.Back.Core.Domain;
+
+namespace UdemyJwtApp.Back.Persistance.Context
+{
+    public class UdemyJwtContext : DbContext
+    {
+        public UdemyJwtContext(DbContextOptions<UdemyJwtContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products => Set<Product>();
+        public DbSet<Category> Categories => Set<Category>();
+        public DbSet<AppUser> AppUsers => Set<AppUser>();
+        public DbSet<AppRole> AppRoles => Set<AppRole>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
